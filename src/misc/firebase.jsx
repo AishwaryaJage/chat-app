1baa39cd3f5ae140d639443893ed07b2d6e958f6
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-15 17:55:41 +0530

    initialized firebase project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDh2fMQzbUn7U2BYEm4JjTB0TTGbAbrRSg',
+  authDomain: 'chat-web-app-abe71.firebaseapp.com',
+  projectId: 'chat-web-app-abe71',
+  storageBucket: 'chat-web-app-abe71.appspot.com',
+  messagingSenderId: '864951969356',
+  appId: '1:864951969356:web:9fa1506b49fc6d029d76e9',
+};
+
+const app = firebase.initializeApp(config);
