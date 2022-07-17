d6c3006639ff95110147ab627e7ce7cfdcca42a0
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-17 20:46:52 +0530

    manage user with profile context;put real time subs

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 function PrivateRoute({ children, ...props }) {
-  const profile = false;
-  console.log({ ...props });
-  if (!profile) {
+  const { isLoading, profile } = useProfile();
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
   return <Route {...props}> {children} </Route>;
---
+++ src/context/profile.context.jsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth, database } from '../misc/firebase';
+
+const ProfileContext = createContext();
+
+export function ProfileProvider({ children }) {
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+
+    const authUnSub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { createdAt, name } = snap.val();
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+          setProfile(data);
+          setisLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        setProfile(null);
+        setisLoading(false);
+      }
+    });
+    return () => {
+      authUnSub();
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{ isLoading, profile }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+}
+
+export const useProfile = () => useContext(ProfileContext);
