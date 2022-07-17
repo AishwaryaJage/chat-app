3e8bb96789f249fcfd6b71ce0cc3819a9ca80887
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-18 00:10:50 +0530

    added signout button and display user name

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { auth } from '../../misc/firebase';
 
 function DashboardToggle() {
   const { isOpen, close, open } = useModalState();
   const is992px = useMediaQuery('(max-width: 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed Out', 4000);
+    close();
+  }, [close]);
 
   return (
     <>
@@ -13,7 +20,7 @@ function DashboardToggle() {
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={is992px} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
