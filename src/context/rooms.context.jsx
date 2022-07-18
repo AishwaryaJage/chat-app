0357f76b3f4ebc43b367dccce9390dc4f43c88aa
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-19 01:30:53 +0530

    created rooms context

---
+++ src/context/rooms.context.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrayWithId } from '../misc/helpers';
+
+const RoomContext = createContext();
+
+export function RoomsProvider({ children }) {
+  const [rooms, setRooms] = useState(null);
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+    roomListRef.on('value', snap => {
+      const data = transformToArrayWithId(snap.val());
+      setRooms(data);
+    });
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+  return (
+    <RoomContext.Provider value={rooms}> {children} </RoomContext.Provider>
+  );
+}
