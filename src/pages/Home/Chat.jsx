1d195aa17b0bc6e1cc3d5dff24bff91ce6a3920d
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-22 14:50:52 +0530

    update security rules for role based permission

--- src/pages/Home/Chat.jsx
+++ src/pages/Home/Chat.jsx
@@ -7,6 +7,8 @@ import Messages from '../../components/chat-window/messages';
 import ChatBottom from '../../components/chat-window/bottom';
 import { useRooms } from '../../context/rooms.context';
 import { CurrentRoomProvider } from '../../context/current-room.context';
+import { transformToArray } from '../../misc/helpers';
+import { auth } from '../../misc/firebase';
 
 function Chat() {
   const { chatId } = useParams();
@@ -23,9 +25,13 @@ function Chat() {
   }
 
   const { name, description } = currentRoom;
+  const admins = transformToArray(currentRoom.admins);
+  const isAdmin = admins.includes(auth.currentUser.uid);
   const currentRoomData = {
     name,
     description,
+    admins,
+    isAdmin,
   };
   return (
     <CurrentRoomProvider data={currentRoomData}>
